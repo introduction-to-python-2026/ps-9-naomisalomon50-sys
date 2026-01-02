06a385c3806327c288a35a8f2e44183891d07645
naomisalomon50-sys <naomi.salomon50@gmail.com>
2026-01-02 17:01:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
+!wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
+import lab_setup_do_not_edit
 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+
+df = pd.read_csv('/content/parkinsons.csv')
+df = df.dropna()
+df
+
+X = df[["HNR", "MDVP:APQ"]]
+y = df["status"]
+
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = DecisionTreeClassifier (max_depth=4,)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy}')
