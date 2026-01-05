805b02dbe16bb2a0faca3dc12bafdb89596b392b
naomisalomon50-sys <naomi.salomon50@gmail.com>
2026-01-05 21:19:48 +0200

    Refactor model training and feature selection

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import MinMaxScaler
 
-df = pd.read_csv('/content/parkinsons.csv')
+df = pd.read_csv("/content/parkinsons.csv")
 df = df.dropna()
-df
 
 print(df.columns.to_list())
-X = df[["HNR", "MDVP:APQ"]]
-y = df["status"]
 
+selected_features = ['RPDE', 'PPE']
+X = df[selected_features]
+y = df['status']
+
+from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 X = scaler.fit_transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-model = DecisionTreeClassifier (max_depth=4,)
-model.fit(X_train, y_train)
+from sklearn.tree import DecisionTreeClassifier
+DTC = DecisionTreeClassifier(max_depth = 3)
+DTC.fit(X_train, y_train)
 
-y_pred = model.predict(X_test)
+from sklearn.metrics import accuracy_score
+y_pred = DTC.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
-print(f'Accuracy: {accuracy}')
+print(accuracy)
